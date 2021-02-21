a919e0e2c007ad142016b2b76411b771f890d287
cota <65804288+cota-eng@users.noreply.github.com>
2021-02-21 20:50:42 +0900

    fix

--- pages/category.tsx
+++ pages/category.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { GetStaticProps } from "next";
 import { getAllCategories } from "../lib/fetchCategory";
 import { CATEGORY } from "../types/category";
+import CategoryDetail from "../components/CategoryDetail";
 
 interface STATICPROPS {
   categories: CATEGORY[];
@@ -10,10 +11,32 @@ interface STATICPROPS {
 
 const brand: React.FC<STATICPROPS> = ({ categories }) => {
   return (
-    <Layout title="ブランドの一覧ページ">
-      <h2>BRAND</h2>
-      {categories &&
-        categories.map((category) => <p key={category.id}>{category.name}</p>)}
+    <Layout title="カテゴリの一覧ページ">
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-col">
+            <div className="h-1 bg-gray-200 rounded overflow-hidden">
+              <div className="w-24 h-full bg-indigo-500"></div>
+            </div>
+            <div className="flex flex-wrap sm:flex-row flex-col py-6 mb-12">
+              <h1 className="sm:w-2/5 text-gray-900 font-medium title-font text-2xl mb-2 sm:mb-0">
+                Category List
+              </h1>
+              <p className="sm:w-3/5 leading-relaxed text-base sm:pl-10 pl-0">
+                Street art subway tile salvia four dollar toast bitters selfies
+                quinoa yuccie synth meditation iPhone intelligentsia prism tofu.
+                Viral gochujang bitters dreamcatcher.
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4">
+            {categories &&
+              categories.map((category) => (
+                <CategoryDetail key={category.id} {...category} />
+              ))}
+          </div>
+        </div>
+      </section>
     </Layout>
   );
 };
