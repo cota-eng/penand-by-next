b88886703fcad393743e1ebb3b2d3fbac736d5e3
cota <65804288+cota-eng@users.noreply.github.com>
2021-02-21 21:02:07 +0900

    add: tag

--- pages/pen/[id].tsx
+++ pages/pen/[id].tsx
@@ -4,7 +4,10 @@ import { GetStaticPaths, GetStaticProps } from "next";
 import Link from "next/link";
 import { getAllPenIds, getPenData } from "../../lib/fetchPen";
 import { useRouter } from "next/router";
+import ReviewForm from "../../components/ReviewForm";
+import ReviewDetail from "../../components/ReviewDetail";
 const PenDetail: React.FC<PEN> = ({
+  id,
   name,
   description,
   category,
@@ -16,21 +19,53 @@ const PenDetail: React.FC<PEN> = ({
   review,
 }) => {
   const router = useRouter();
-  if (router.isFallback || !name) {
+  if (router.isFallback || !id) {
     return <div>loading...</div>;
   }
   // pen.tsxからのpropsを分解しているから、nameがないときはpenがないと同じはず
   return (
     <div>
-      <Layout title="fff">
-        <p>{name}</p>
+      <Layout title="{name}のレビュー">
+        <section className="text-gray-600 body-font">
+          <div className="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
+            <a
+              className="inline-block rounded-full text-white 
+        bg-blue-400 hover:bg-blue-500 duration-300 
+        text-xs font-bold 
+        mr-1 md:mr-2 mb-2 px-2 md:px-4 py-1 
+        opacity-90 hover:opacity-100"
+            >
+              カテゴリ：{category.name}
+            </a>
+            <img
+              className="lg:w-6/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded"
+              alt="hero"
+              src="https://dummyimage.com/720x600"
+            />
+            <div className="text-center lg:w-2/3 w-full">
+              <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+                {name}
+              </h1>
+              <p className="mb-8 leading-relaxed">{brand.name}.</p>
+              <div className="flex justify-center">
+                <a className="inline-flex text-white bg-gray-500 border-0 py-2 px-6  rounded text-lg">
+                  {created_at}
+                </a>
+                <a className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6  rounded text-lg">
+                  {updated_at}
+                </a>
+              </div>
+              <div className="flex justify-center">
+                <button className="inline-flex text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded text-lg">
+                  Button
+                </button>
+              </div>
+            </div>
+          </div>
+        </section>
         <p>{description}</p>
-        <p>{category.name}</p>
-        <p>{price_yen}</p>
-        <p>{brand.name}</p>
-        <p>{tag.name}</p>
-        <p>{created_at}</p>
-        <p>{updated_at}</p>
+        <p>{price_yen}円</p>
+        <p>{tag && tag.map((t) => <p key={t.id}>{t.name}</p>)}</p>
         <Link href="/pen">
           <div className="flex cursor-pointer mt-12">
             <svg
@@ -47,12 +82,16 @@ const PenDetail: React.FC<PEN> = ({
                 d="M11 19l-7-7 7-7m8 14l-7-7 7-7"
               />
             </svg>
-            <a>"Go back to Home"</a>
+            <a className="text-lg">Home</a>
           </div>
         </Link>
-        <p>review</p>
-
-        {review && review.map((rev) => <p key={rev.id}>{rev.title}</p>)}
+        <hr className="text-black-200" />
+        {review &&
+          review.map((rev) => (
+            <ReviewDetail key={rev.id} {...rev}></ReviewDetail>
+          ))}
+        <hr />
+        <ReviewForm id={id} />
       </Layout>
     </div>
   );
@@ -71,7 +110,7 @@ export const getStaticProps: GetStaticProps = async (context) => {
   const pen = await getPenData(context.params.id as string);
   return {
     props: { ...pen },
-    revalidate: 3,
+    revalidate: 10,
   };
 };
 
