bef72c07e8fdc76b58c839aa4182e9a67a6834c9
cota <65804288+cota-eng@users.noreply.github.com>
2021-02-22 15:40:16 +0900

    fin

--- components/Tag.tsx
+++ components/Tag.tsx
@@ -3,9 +3,17 @@ import { TAG } from "../types/tag";
 
 const Tag: React.FC<TAG> = ({ name }) => {
   return (
-    <div>
-      <p>{name}</p>
-    </div>
+    <>
+      <span
+        className="inline rounded-full text-white 
+        bg-black hover:bg-gray-500 duration-300 
+        text-xs font-bold 
+        mr-1 md:mr-2 mb-2 px-2 md:px-4 py-1 
+        opacity-90 hover:opacity-100"
+      >
+        {name}
+      </span>
+    </>
   );
 };
 
