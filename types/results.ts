c5c0893c558e509837f1c42f43ca138441b6973a
cota <65804288+cota-eng@users.noreply.github.com>
2021-04-02 21:24:28 +0900

    :fire: add type

--- types/results.ts
+++ types/results.ts
@@ -1,8 +1,8 @@
-import { PEN } from "./product";
+import { PRODUCT } from "./product";
 
 export interface RESULTS {
   count: number | null;
   next: string | null;
   previous: string | null;
-  results: PEN[];
+  results: PRODUCT[] | null;
 }
