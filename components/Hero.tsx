f8d1be2a81f47cf69f5fb89fdf40320d0ecfbc44
cota <65804288+cota-eng@users.noreply.github.com>
2021-04-02 12:22:00 +0900

    customed

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -9,25 +9,22 @@ const Hero = () => {
   };
   return (
     <>
-      <div className="pt-24">
+      <div className="pt-24 mx-auto">
         <button onClick={_showSnackbarHandler}>
           Click To Open To Snackbar
         </button>
         <SnackBar isActive={isActive} message={message} />
-        <div className="container px-3 mx-auto flex flex-wrap flex-col md:flex-row items-center">
-          <div className="flex flex-col w-full md:w-2/5 justify-center items-start text-center md:text-left">
+        <div className="container px-3">
+          <div className="flex flex-col w-full md:w-4/5  text-center md:text-left">
             <p className="uppercase tracking-loose w-full">
               より正確なレビューをしませんか？
             </p>
-            <button className="transition duration-500 ease-in-out bg-blue-600 hover:bg-red-600 transform hover:-translate-y-1 hover:scale-110 bg-blue-500  hover:py-10 text-white font-bold py-2 px-4 rounded ">
-              Hover me
-            </button>
             <h1 className="my-4 text-5xl font-bold leading-tight">PENAND</h1>
             <p className="leading-normal text-2xl mb-8">
               「より役に立つ文房具レビューのできるサイトです」
             </p>
-            <Link href="/login/">
-              <a className="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
+            <Link href="/login">
+              <a className="mx-auto lg:mx-0 text-center w-40  bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
                 LOGIN
               </a>
             </Link>
