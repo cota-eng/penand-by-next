6884c99db46b9f397f840fc96da133ad12b075ea
cota <65804288+cota-eng@users.noreply.github.com>
2021-04-02 19:34:42 +0900

    add setting page

--- pages/account/setting.tsx
+++ pages/account/setting.tsx
@@ -1,6 +1,14 @@
 import { NextPage } from "next";
+import Layout from "../../components/Layout";
+import { useCurrentUser } from "../../hooks/useCurrentUser";
+import { useRequireLogin } from "../../hooks/useRequireLogin";
 const setting: NextPage = () => {
-  return <div></div>;
+  //   useRequireLogin();
+  const { isAuthChecking, currentUser } = useCurrentUser();
+  if (isAuthChecking) {
+    return <Layout title="Now Login...">LOGIN</Layout>;
+  }
+  return <Layout title="setting">setting</Layout>;
 };
 
 export default setting;
