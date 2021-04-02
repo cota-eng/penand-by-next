4c974ee01d88b390021c770ed99eb0190c45342c
cota <65804288+cota-eng@users.noreply.github.com>
2021-04-02 23:11:08 +0900

    :sparcle: process improved

---
+++ lib/auth/googleLogin.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+// import { useRouter } from "next/router";
+import Cookies from "universal-cookie";
+// const cookies = new Cookies();
+// const router = useRouter();
+const googleLogin = async (accesstoken: string) => {
+  const res = await axios.post(
+    `${process.env.NEXT_PUBLIC_API_URL}/api/login/`,
+    {
+      withCredentials: true,
+      access_token: accesstoken,
+    }
+  );
+  return res
+//   if (res.status === 200) {
+//     localStorage.setItem("access_token", JSON.stringify(res.data.access_token));
+//     localStorage.setItem(
+//       "refresh_token",
+//       JSON.stringify(res.data.refresh_token)
+//     );
+//   } else {
+//     console.log("not 200 OK");
+//   }
+};
+
+export default googleLogin;
