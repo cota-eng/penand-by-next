b2b288b67a41fd0916c1b2e73f1eab4c349b4d53
cota <65804288+cota-eng@users.noreply.github.com>
2021-04-02 23:11:28 +0900

    :+1: add sidebar

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -2,19 +2,31 @@ import React from "react";
 
 const SideBar: React.FC = () => {
   return (
-    <aside className="h-screen sticky top-0 pt-20 overflow-auto h-auto w-0 sm:w-0 md:w-0 lg:w-1/5 xl:w-1/5  bg-gray-100">
-      <h2>カテゴリ</h2>
-      <h2>ブランド</h2>
-      <h2>カテゴリ</h2>
-      <h2>ブランド</h2>
-      <h2>カテゴリ</h2>
-      <h2>ブランド</h2>
-      <h2>カテゴリ</h2>
-      <h2>ブランド</h2>
-      <h2>カテゴリ</h2>
-      <h2>ブランド</h2>
-      <h2>カテゴリ</h2>
-      <h2>ブランド</h2>
+    <aside className="h-screen sticky top-0 pt-10 pl-10 overflow-auto h-auto w-0 sm:w-0 md:w-0 lg:w-1/5 xl:w-1/5  bg-gray-100">
+      <h2 className="font-semibold mb-2 text-3xl leading-tight sm:leading-normal">
+        メニュー
+      </h2>
+      <h2 className="font-semibold mb-2 mt-5 text-xl leading-tight sm:leading-normal">
+        カテゴリ
+      </h2>
+      <p>シャーペン</p>
+      <p>ボールペン</p>
+      <p>万年筆</p>
+      <p>消しゴム</p>
+      <p>シャーペン</p>
+      <p>シャーペン</p>
+      <h2 className="font-semibold mb-2 mt-5 text-xl leading-tight sm:leading-normal">
+        ブランド
+      </h2>
+      <p>ブランド</p>
+      <p>ブランド</p>
+      <p>ブランド</p>
+      <p>ブランド</p>
+      <p>ブランド</p>
+      <p>ブランド</p>
+      <h2 className="font-semibold mb-2 mt-5 text-xl leading-tight sm:leading-normal">
+        タグ
+      </h2>
     </aside>
   );
 };
