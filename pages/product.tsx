81af8ce8a11f269a29e4e9521d4ca350e0899388
cota <65804288+cota-eng@users.noreply.github.com>
2021-04-02 23:18:56 +0900

    :memo: css customed

--- pages/product.tsx
+++ pages/product.tsx
@@ -7,24 +7,26 @@ import ReactMarkdown from "react-markdown";
 import Product from "../components/Product";
 import { useEffect } from "react";
 import { animateScroll as scroll } from "react-scroll";
+import { NextPage } from "next";
 
 interface STATICPROPS {
   products: PRODUCT[];
 }
 
-const product: React.FC<STATICPROPS> = ({ products }) => {
-//   useEffect(() => {
-//     scroll.scrollToBottom();
-//   }, []);
+const product: NextPage<STATICPROPS> = ({ products }) => {
+  //   useEffect(() => {
+  //     scroll.scrollToBottom();
+  //   }, []);
   return (
     <Layout title="ペンの一覧ページ">
-      <div>
-        <h2 className="font-semibold mb-2 mx-auto mt-5 leading-tight text-3xl w-full text-gray-500">
+      <div className="overflow-hidden">
+        <h2 className="font-semibold mb-2 mx-auto text-center mt-5 text-3xl w-full text-gray-500">
           ペンの一覧ページ
         </h2>
+        <div>description</div>
       </div>
       <div>
-        <section className="text-gray-600 body-font">
+        <section className="text-gray-600 body-font overflow-auto h-auto">
           <div className="container px-5 py-24 mx-auto">
             <div className="flex flex-wrap -m-2">
               {products &&
