e21394eb76d7875845224db814c08893c7d840fd
cota <65804288+cota-eng@users.noreply.github.com>
2021-04-02 17:16:59 +0900

    :memo: changed position

--- hooks/useCurrentUser.tsx
+++ hooks/useCurrentUser.tsx
@@ -12,7 +12,7 @@ export function useCurrentUser() {
   const currentUser = useRecoilValue(currentUserState);
   const isAuthChecking = currentUser === undefined;
   return {
-    currentUser,
     isAuthChecking,
+    currentUser,
   };
 }
