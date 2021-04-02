0d56cb2b5ea26eab7fe8506024346097c4b6a9c7
cota <65804288+cota-eng@users.noreply.github.com>
2021-04-02 16:45:25 +0900

    :fire: add sidebar

---
+++ components/SideBar.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const SideBar: React.FC = () => {
+  return (
+    <aside className="h-screen sticky top-0 pt-20 overflow-auto h-auto w-0 sm:w-0 md:w-0 lg:w-1/5 xl:w-1/5  bg-gray-100">
+      <h2>カテゴリ</h2>
+      <h2>ブランド</h2>
+      <h2>カテゴリ</h2>
+      <h2>ブランド</h2>
+      <h2>カテゴリ</h2>
+      <h2>ブランド</h2>
+      <h2>カテゴリ</h2>
+      <h2>ブランド</h2>
+      <h2>カテゴリ</h2>
+      <h2>ブランド</h2>
+      <h2>カテゴリ</h2>
+      <h2>ブランド</h2>
+    </aside>
+  );
+};
+
+export default SideBar;
