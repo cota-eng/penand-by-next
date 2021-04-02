a83d46c4d3916e4d73e91f91ef8cbbc2a94224f9
cota <65804288+cota-eng@users.noreply.github.com>
2021-04-02 17:21:08 +0900

    :memo: type Page

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,12 +1,26 @@
 import Layout from "../components/Layout";
 import GoogleSocialAuth from "../components/Authentication/GoogleSocialAuth";
 import { useRequireLogin } from "../hooks/useRequireLogin";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { currentUserState } from "../states/currentUserState";
+import { useCurrentUser } from "../hooks/useCurrentUser";
+import { NextPage } from "next";
 
-const login: React.FC = () => {
+const login: NextPage = () => {
   useRequireLogin();
+  useCurrentUser();
+  const { isAuthChecking } = useCurrentUser();
+  const currentUser = useRecoilValue(currentUserState);
+//   if (currentUser) {
+//     console.log(currentUser["id"]);
+//   }
+  if (isAuthChecking) return <div>ログイン情報を確認中…</div>;
+  if (!currentUser) return <div>ログインしていません</div>;
   return (
     <Layout title="ログイン">
       <GoogleSocialAuth />
+      <p>{!currentUser && "no user"}</p>
+      <p>{currentUser && currentUser.id}</p>
     </Layout>
   );
 };
