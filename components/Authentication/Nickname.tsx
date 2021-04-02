0fc7463785eea92be9239dadd1a3266d6b38e353
cota <65804288+cota-eng@users.noreply.github.com>
2021-04-03 08:29:24 +0900

    changed

--- components/Authentication/Nickname.tsx
+++ components/Authentication/Nickname.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import { useEffect,useState } from "react";
+import { useForm } from "react-hook-form";
+
+type Inputs = {
+  nickname: string;
+};
 
 const Nickname: React.FC = () => {
-  return <div>{/* for change nickname */}</div>;
+  const { register, handleSubmit, watch, errors } = useForm<Inputs>();
+  useEffect(() => {
+    //   const res = fetch()
+  },[])
+  const [nickname,setNickname]=useState<string>("")
+    return <div>
+        {/* <form onSubmit={handleSubmit(onSubmit)}>
+            <input name="nicjname" defaultValue=
+      </form> */}
+  </div>;
 };
 
 export default Nickname;
--- pages/login.tsx
+++ pages/login.tsx
@@ -7,15 +7,16 @@ import { useCurrentUser } from "../hooks/useCurrentUser";
 import { NextPage } from "next";
 
 const login: NextPage = () => {
-  useRequireLogin();
+  //   useRequireLogin();
   useCurrentUser();
   const { isAuthChecking } = useCurrentUser();
   const currentUser = useRecoilValue(currentUserState);
-//   if (currentUser) {
-//     console.log(currentUser["id"]);
-//   }
+  //   if (currentUser) {
+  //     console.log(currentUser["id"]);
+  //   }
   if (isAuthChecking) return <div>ログイン情報を確認中…</div>;
-  if (!currentUser) return <Layout title="need to login">ログインしていません(spinner)</Layout>;
+  //   if (!currentUser)
+  //     return <Layout title="need to login">ログインしていません(spinner)</Layout>;
   return (
     <Layout title="ログイン">
       <GoogleSocialAuth />
