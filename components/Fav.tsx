99e7af3d5a2e420a640873dc224afab82336fd3a
cota <65804288+cota-eng@users.noreply.github.com>
2021-04-03 15:59:44 +0900

    :tada: implemented a model

--- components/Fav.tsx
+++ components/Fav.tsx
@@ -1,11 +1,14 @@
 import axios from "axios";
+import Link from "next/link";
 import { useState, useEffect } from "react";
 import { useCurrentUser } from "../hooks/useCurrentUser";
+// import Modal from "react-modal";
 interface FAVPROPS {
   id: string;
 }
 const Fav: React.FC<FAVPROPS> = ({ id }) => {
   const [isFav, setIsFav] = useState(false);
+  const [isFavModalOpen, setIsFavModalOpen] = useState(false);
   const { isAuthChecking, currentUser } = useCurrentUser();
   useEffect(() => {
     if (!currentUser) {
@@ -27,7 +30,7 @@ const Fav: React.FC<FAVPROPS> = ({ id }) => {
       fetchFav();
     }
   }, []);
- 
+
   const favChange = () => {
     if (!currentUser) {
       return null;
@@ -53,7 +56,9 @@ const Fav: React.FC<FAVPROPS> = ({ id }) => {
     <div>
       <button
         className="text-pink-500 background-transparent font-bold uppercase px-3 py-1 text-xs outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-300"
-        onClick={() => favChange()}
+        onClick={
+          currentUser ? () => favChange() : () => setIsFavModalOpen(true)
+        }
       >
         <svg
           className="w-6 h-6"
@@ -70,6 +75,56 @@ const Fav: React.FC<FAVPROPS> = ({ id }) => {
           />
         </svg>
       </button>
+      {isFavModalOpen ? (
+        <>
+          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
+                  <h3 className="text-3xl font-semibold">
+                    <span className="text-lx">いいね</span>
+                    するにはログインが必要です
+                  </h3>
+                  <button
+                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    onClick={() => setIsFavModalOpen(false)}
+                  >
+                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                      ×
+                    </span>
+                  </button>
+                </div>
+                {/*body*/}
+                <div className="relative p-6 flex-auto">
+                  <p className="my-4 text-blueGray-500 text-lg leading-relaxed">
+                    「いいね」をした商品は、プロフィール画像タップしたメニューから一覧できます。
+                  </p>
+                </div>
+                {/*footer*/}
+                <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
+                  <button
+                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setIsFavModalOpen(false)}
+                  >
+                    Close
+                  </button>
+                  <button
+                    className="bg-blue-300 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    //   onClick={() => setIsFavModalOpen(false)}
+                  >
+                    <Link href="/login">Login</Link>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
     </div>
   );
 };
