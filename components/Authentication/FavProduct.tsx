40f440b39fdf9e51190921eec3ae4d8f4ecc470c
cota <65804288+cota-eng@users.noreply.github.com>
2021-04-04 16:55:29 +0900

    :tada: implement fav page

--- components/Authentication/FavProduct.tsx
+++ components/Authentication/FavProduct.tsx
@@ -1,11 +1,43 @@
-import {useState,useEffect} from 'react'
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { useCurrentUser } from "../../hooks/useCurrentUser";
+import { useRequireLogin } from "../../hooks/useRequireLogin";
+import { FAVPRODUCT } from "../../types/fav";
 
-const FavProduct:React.FC = () => {
-    return (
-        <div>
-            fav
-        </div>
-    )
-}
+const FavProduct: React.FC = () => {
+  // ログイン必須、CSR、1つ1つをProductCompへ代入
+  const { isAuthChecking, currentUser } = useCurrentUser();
+  const [favProducts, setFavProducts] = useState<FAVPRODUCT[] | null>();
+  useRequireLogin();
+  useEffect(() => {
+    if (!currentUser) {
+      return null;
+    } else {
+      const fetchFavProducts = async () => {
+        const res = await axios.get(
+          `${process.env.NEXT_PUBLIC_API_URL}/api/fav-list/`,
+          {
+            headers: {
+              Authorization: `Bearer ${JSON.parse(
+                localStorage.getItem("access_token")
+              )}`,
+            },
+          }
+        );
+        setFavProducts(res.data);
+      };
+      fetchFavProducts();
+    }
+  }, []);
+  return (
+    <>
+      <p>fav</p>
+      {favProducts &&
+        favProducts.map((favProduct) => (
+          <p key={favProduct.product.id}>{favProduct.product.name}</p>
+        ))}
+    </>
+  );
+};
 
-export default FavProduct
+export default FavProduct;
