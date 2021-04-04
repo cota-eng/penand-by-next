2075cff4126fce227c16b2a27a842105440e972e
cota <65804288+cota-eng@users.noreply.github.com>
2021-04-04 22:09:08 +0900

    :+1: add todo

--- pages/login.tsx
+++ pages/login.tsx
@@ -14,15 +14,15 @@ const login: NextPage = () => {
   const currentUser = useRecoilValue(currentUserState);
   useEffect(() => {
     if (currentUser) {
-        router.push("/");
-        // TODO: how to move to previous page
+      router.push("/");
+      // TODO: how to move to previous page
     }
   }, [currentUser]);
   if (isAuthChecking) return <div>ログイン情報を確認中…</div>;
   //   if (!currentUser)
   //     return <Layout title="need to login">ログインしていません(spinner)</Layout>;
   return (
-    <Layout title="ログイン">
+    <Layout title="ログイン" >
       <GoogleSocialAuth />
       <p>{!currentUser && "未ログインです。"}</p>
       <p>{currentUser && currentUser.id}</p>
