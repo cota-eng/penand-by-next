3648aa80a81521f313228036ebfffa62f891cf43
cota <65804288+cota-eng@users.noreply.github.com>
2021-04-04 22:08:40 +0900

    :bug: css

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const SideBar: React.FC = () => {
   return (
-    <aside className="h-screen sticky top-0 pt-10 pl-10 overflow-auto h-auto w-0 sm:w-0 md:w-0 lg:w-1/5 xl:w-1/5  bg-gray-100">
+    <aside className="h-screen sticky top-0 pt-10 pl-10 overflow-auto hidden w-0 lg:w-1/5 lg:block xl:w-1/5  bg-gray-100">
       <h2 className="font-semibold mb-2 text-3xl leading-tight sm:leading-normal">
         メニュー
       </h2>
