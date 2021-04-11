f81959cbe6cb53abde61896e85226b46c30b1cfa
cota <65804288+cota-eng@users.noreply.github.com>
2021-04-11 14:13:43 +0900

    add fetch lib

--- lib/fetchProducts.ts
+++ lib/fetchProducts.ts
@@ -29,3 +29,28 @@ export const getProductData = async (id: string) => {
   const product = await res.json();
   return product;
 };
+
+export const getCategoryBrandId = async () => {
+  //   const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/products/`);
+  //   const products: PRODUCT[] = await res.json();
+  //   return products.map((product: PRODUCT) => {
+  //     return {
+  //       params: {
+  //         id: product.id.toString(),
+  //       },
+  //     };
+  //   });
+};
+
+export const getCategoryBrandFilterdProductData = async (
+  category_slug: string,
+  brand_slug: string
+) => {
+  const res = await fetch(
+    new URL(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/category/${category_slug}/brand/${brand_slug}/`
+    )
+  );
+  const products = await res.json();
+  return products;
+};
