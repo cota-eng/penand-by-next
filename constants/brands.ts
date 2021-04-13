4fe97bfac918c4b2af6ba2a406c8e99728b71ed1
cota <65804288+cota-eng@users.noreply.github.com>
2021-04-13 15:25:46 +0900

    constant

--- constants/brands.ts
+++ constants/brands.ts
@@ -1,3 +1,34 @@
-export default {
-  "1": "mechanical",
-};
+export default [
+  {
+    name: "カランダッシュ",
+    slug: "carandache",
+  },
+  {
+    name: "ファーバーカステル",
+    slug: "faber-castell",
+  },
+  {
+    name: "ラミー",
+    slug: "lamy",
+  },
+  {
+    name: "オート",
+    slug: "ohto",
+  },
+  {
+    name: "ぺんてる",
+    slug: "pentel",
+  },
+  {
+    name: "トンボ",
+    slug: "tombow",
+  },
+  {
+    name: "ユニ",
+    slug: "uni",
+  },
+  {
+    name: "ゼブラ",
+    slug: "zebra",
+  },
+];
