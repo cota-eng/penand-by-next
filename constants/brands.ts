fb5c42ec67d1e138244d46569cb51a6a53cd6a15
cota <65804288+cota-eng@users.noreply.github.com>
2021-04-13 16:54:05 +0900

    constants

--- constants/brands.ts
+++ constants/brands.ts
@@ -1,4 +1,4 @@
-export default [
+export  const brands =  [
   {
     name: "カランダッシュ",
     slug: "carandache",
---
+++ constants/slugs.tsx
@@ -0,0 +1,67 @@
+export const slugs = [
+    // TODO fetchからデータ整形に置き換える
+  {
+    category: "mechanical",
+    brand: "carandache",
+  },
+  {
+    category: "mechanical",
+    brand: "faber-castell",
+  },
+  {
+    category: "mechanical",
+    brand: "lamy",
+  },
+  {
+    category: "mechanical",
+    brand: "ohto",
+  },
+  {
+    category: "mechanical",
+    brand: "pentel",
+  },
+  {
+    category: "mechanical",
+    brand: "tombow",
+  },
+  {
+    category: "mechanical",
+    brand: "uni",
+  },
+  {
+    category: "mechanical",
+    brand: "zebra",
+  },
+  {
+    category: "ballpoint",
+    brand: "carandache",
+  },
+  {
+    category: "ballpoint",
+    brand: "faber-castell",
+  },
+  {
+    category: "ballpoint",
+    brand: "lamy",
+  },
+  {
+    category: "ballpoint",
+    brand: "ohto",
+  },
+  {
+    category: "ballpoint",
+    brand: "pentel",
+  },
+  {
+    category: "ballpoint",
+    brand: "tombow",
+  },
+  {
+    category: "ballpoint",
+    brand: "uni",
+  },
+  {
+    category: "ballpoint",
+    brand: "zebra",
+  },
+];
