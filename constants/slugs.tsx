419df782482935f172090a3c43936980b0618bb4
cota <65804288+cota-eng@users.noreply.github.com>
2021-04-13 17:59:29 +0900

    一時的に手動データ

--- constants/slugs.tsx
+++ constants/slugs.tsx
@@ -1,67 +1,83 @@
 export const slugs = [
-    // TODO fetchからデータ整形に置き換える
+  // TODO 他のconstantデータから、その整形に置き換える
   {
     category: "mechanical",
     brand: "carandache",
+    count: 42,
   },
   {
     category: "mechanical",
     brand: "faber-castell",
+    count: 11,
   },
   {
     category: "mechanical",
     brand: "lamy",
+    count: 22,
   },
   {
     category: "mechanical",
     brand: "ohto",
+    count: 8,
   },
   {
     category: "mechanical",
     brand: "pentel",
+    count: 102,
   },
   {
     category: "mechanical",
     brand: "tombow",
+    count: 7,
   },
   {
     category: "mechanical",
     brand: "uni",
+    count: 413,
   },
   {
     category: "mechanical",
     brand: "zebra",
+    count: 19,
   },
   {
     category: "ballpoint",
     brand: "carandache",
+    count: 60,
   },
   {
     category: "ballpoint",
     brand: "faber-castell",
+    count: 16,
   },
   {
     category: "ballpoint",
     brand: "lamy",
+    count: 79,
   },
   {
     category: "ballpoint",
     brand: "ohto",
+    count: 21,
   },
   {
     category: "ballpoint",
     brand: "pentel",
+    count: 171,
   },
   {
     category: "ballpoint",
     brand: "tombow",
+    count: 12,
   },
   {
     category: "ballpoint",
     brand: "uni",
+    count: 854,
   },
   {
     category: "ballpoint",
     brand: "zebra",
+    count: 49,
   },
 ];
