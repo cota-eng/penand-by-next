59eabe69a86387a3baf52e8b0690ea4f4593d424
cota <65804288+cota-eng@users.noreply.github.com>
2021-04-13 14:43:15 +0900

    add categories

---
+++ constants/categories.tsx
@@ -0,0 +1,14 @@
+export default [
+  {
+    name: "シャーペン",
+    slug: "mechanical",
+  },
+  {
+    name: "ボールペン",
+    slug: "ballpoint",
+  },
+  {
+    name: "万年筆",
+    slug: "fountain",
+  },
+];
