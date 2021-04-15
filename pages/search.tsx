20918cc733592a2adc19878b7e4326ddb0e4d0c8
cota <65804288+cota-eng@users.noreply.github.com>
2021-04-16 08:35:25 +0900

    searching page

--- pages/search.tsx
+++ pages/search.tsx
@@ -10,15 +10,7 @@ import PenResult from "../components/ProductResult";
 import { PRODUCT } from "../types/product";
 import Product from "../components/Product";
 import axios from "axios";
-// const CategorySelect = dynamic(() => import("../components/Search/Category"), {
-//   ssr: false,
-// });
-// const TagSelect = dynamic(() => import("../components/Search/Tag"), {
-//   ssr: false,
-// });
-// const BrandSelect = dynamic(() => import("../components/Search/Brand"), {
-//   ssr: false,
-// });
+
 import Autosuggest from "react-autosuggest";
 import { suggest } from "../constants/suggest";
 import { SUGGESTINPUT } from "../types/suggestInput";
@@ -37,15 +29,7 @@ const getSuggestions = (value: string): SUGGESTINPUT[] => {
 const Search: React.FC = () => {
   const [value, setValue] = useState("");
   const router = useRouter();
-  const [products, setProducts] = useState<PRODUCT[]>(null);
   const [isSearch, setIsSearch] = useState(false);
-  //   const [minPrice, setMinPrice] = useState<string | undefined>(null);
-  //   const [maxPrice, setMaxPrice] = useState<string | undefined>(null);
-  //   const resetKeyword = () => {
-  //     setName("");
-  //     setMinPrice("0");
-  //     setMaxPrice("100000");
-  //   }; const [value, setValue] = useState("");
   const [suggestions, setSuggestions] = useState<SUGGESTINPUT[]>([]);
 
   const getSuggestionValue = (suggestion: SUGGESTINPUT): string => {
@@ -56,11 +40,13 @@ const Search: React.FC = () => {
 
   const renderSuggestion = (suggestion: SUGGESTINPUT) => {
     return (
-      <div className="text-sm to-blue-200 text-center">
-        <Link href={`product/${suggestion.pk}`}>
-          <a target="_blank">{suggestion.name}</a>
-        </Link>
-      </div>
+      <>
+        <div className="text-sm underline to-blue-200 text-left  mt-1">
+          <Link href={`product/${suggestion.pk}`}>
+            <a target="_blank">{suggestion.name}</a>
+          </Link>
+        </div>
+      </>
     );
   };
 
@@ -114,12 +100,12 @@ const Search: React.FC = () => {
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900 text-center">
             SEARCH
           </h1>
-          <div className="w-1/2 mx-auto">
+          <div className="md:w-1/2 w-3/4 mx-auto">
             <div className="bg-gray-200 shadow-md rounded-lg px-3 py-2 mb-4 text-center mx-auto">
               {/* <div className="flex items-center bg-white rounded-md mt-5 mx-auto"> */}
-              <div className="">
+              <div className="flex  mb-4">
                 <svg
-                  className="fill-current text-gray-500 w-6 h-6"
+                  className="fill-current text-gray-500 w-5 h-5"
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 24 24"
                 >
@@ -128,11 +114,11 @@ const Search: React.FC = () => {
                     d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z"
                   />
                 </svg>
-                <p className="title-font  mb-4 font-medium text-gray-900 text-center">
+                <p className="title-font text-sm  font-medium text-gray-900 text-center">
                   タップすると新しいタブで開かれます。
                 </p>
               </div>
-              <div className=" mx-auto">
+              <div className="mx-0 flex justify-items-center">
                 <Autosuggest
                   suggestions={suggestions}
                   onSuggestionsFetchRequested={onSuggestionsFetchRequested}
@@ -141,89 +127,41 @@ const Search: React.FC = () => {
                   renderSuggestion={renderSuggestion}
                   inputProps={inputProps}
                 />
+                <svg
+                  className="w-6 h-6 ml-2"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M12 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2M3 12l6.414 6.414a2 2 0 001.414.586H19a2 2 0 002-2V7a2 2 0 00-2-2h-8.172a2 2 0 00-1.414.586L3 12z"
+                  />
+                </svg>
               </div>
-              {/* <input
-                  className="w-full rounded-md bg-gray-200 text-gray-700 leading-tight focus:outline-none py-2 px-2"
-                  id="search"
-                  type="text"
-                  onChange={(e) => setName(e.target.value)}
-                  placeholder="Search For Name"
-                /> */}
-              {/* </div> */}
+
               <div className="py-3 text-sm">
                 {/* <TagSelect setTag={setTag} /> */}
               </div>
-              {/* <div className="flex items-center justify-center">
-                <div className="inline-flex items-center mt-2 w-1/3 mr-1  border border-gray-700">
-                  <label
-                    className="hidden  text-sm text-gray-600"
-                    htmlFor="min"
-                  ></label>
-                  <input
-                    className="w-full px-2 py-2 text-gray-700  rounded"
-                    name="min"
-                    type="number"
-                    required={true}
-                    value={minPrice ? minPrice : "0"}
-                    onChange={(e) => setMinPrice(e.target.value)}
-                    placeholder="最低金額"
-                    aria-label="number"
-                  />
-                </div>
-                {"~"}
-                <div className="inline-flex items-center mt-2 -mx-1 ml-1 w-2/5  mr-1 border border-gray-700">
-                  <label
-                    className="hidden text-sm text-gray-600"
-                    htmlFor="max"
-                  ></label>
-                  <input
-                    className="w-full px-2 py-2 text-gray-700  rounded"
-                    name="max"
-                    type="number"
-                    required={true}
-                    value={maxPrice ? maxPrice : "100000"}
-                    onChange={(e) => setMaxPrice(e.target.value)}
-                    placeholder="最高金額"
-                    aria-label="number"
-                  />
-                </div>
-              </div> */}
-              {/* <div className="flex items-center justify-center">
-                <div className="m-3">
-                  <button
-                    className="bg-white text-gray-800 font-bold rounded border-b-2 border-green-500 hover:border-green-600 hover:bg-green-500 hover:text-white shadow-md py-2 px-6 inline-flex items-center"
-                    type="submit"
-                    onClick={() => setIsSearch(!isSearch)}
-                  >
-                    <span className="">SEARCH</span>
-                  </button>
-                </div>
-              </div> */}
             </div>
           </div>
         </div>
-        {/* <section className="text-gray-600 body-font">
-          <div className="container px-5 py-24 mx-auto">
-            <div className="flex flex-wrap -m-2">
-              {products &&
-                products.map((product) => (
-                  <Product key={product.id} {...product} />
-                ))}
-              {!products && <h2>該当するペンはありませんでした</h2>}
-            </div>
-          </div>
-        </section> */}
       </div>
       <style jsx>{`
+        input {
+          width: full;
+        }
         .react-autosuggest__container {
           position: relative;
         }
 
         .react-autosuggest__input {
-          width: 240px;
+          width: 200px;
           height: 30px;
           padding: 10px 20px;
-          font-family: "Open Sans", sans-serif;
           font-weight: 300;
           font-size: 16px;
           border: 1px solid #aaa;
