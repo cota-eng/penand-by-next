b50cd94ad0be0dd23adfb7bee0e5ac618eb638f4
cota <65804288+cota-eng@users.noreply.github.com>
2021-04-16 08:35:41 +0900

    changed

--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -50,7 +50,7 @@ const ProductPage: React.FC<PRODUCT> = ({
                             bg-${color}-400 hover:bg-blue-500 duration-300 
                         text-xs font-bold 
                         mr-1 md:mr-2 mb-2 px-2 md:px-4 py-1 
-                        opacity-90 hover:opacity-100`}
+                         hover:opacity-100`}
             >
               カテゴリ：{category.name}
             </span>
