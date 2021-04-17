f7fe0ae8fdfde821c2a5fa8f82b3a3e1dbd7c887
cota <65804288+cota-eng@users.noreply.github.com>
2021-04-17 09:34:23 +0900

    added constants

--- constants/tags.ts
+++ constants/tags.ts
@@ -1,15 +1,125 @@
 import { TAG } from "../types/tag";
 interface CONSTTAG {
-  id: string;
+  pk: number;
   name: string;
   slug: string;
-  emoji: string;
+  //   emoji: string;
 }
 export const tags: CONSTTAG[] = [
   {
-    id: "01",
-    name: "wood",
+    pk: 1,
+    name: "木軸",
     slug: "wood",
-    emoji: ":wood:",
+  },
+  {
+    pk: 2,
+    name: "高級",
+    slug: "rich",
+  },
+  {
+    pk: 3,
+    name: "安価",
+    slug: "low-price",
+  },
+  {
+    pk: 4,
+    name: "ラバー",
+    slug: "rubber",
+  },
+  {
+    pk: 5,
+    name: "おしゃれ",
+    slug: "fashionable",
+  },
+  {
+    pk: 6,
+    name: "限定",
+    slug: "limited",
+  },
+  {
+    pk: 7,
+    name: "金属",
+    slug: "metal",
+  },
+  {
+    pk: 8,
+    name: "ベストセラー",
+    slug: "best-seller",
+  },
+  {
+    pk: 9,
+    name: "カラフル",
+    slug: "colorful",
+  },
+  {
+    pk: 10,
+    name: "多機能",
+    slug: "functional",
+  },
+  {
+    pk: 11,
+    name: "新商品",
+    slug: "new",
+  },
+  {
+    pk: 12,
+    name: "製図用",
+    slug: "draft",
+  },
+  {
+    pk: 13,
+    name: "多色",
+    slug: "full-color",
+  },
+
+  {
+    pk: 14,
+    name: "日本",
+    slug: "japan",
+  },
+  {
+    pk: 15,
+    name: "海外",
+    slug: "foreign",
+  },
+  {
+    pk: 16,
+    name: "クリップなし",
+    slug: "no-grip",
+  },
+  {
+    pk: 17,
+    name: "軽い",
+    slug: "light",
+  },
+  {
+    pk: 18,
+    name: "重い",
+    slug: "heavy",
+  },
+  {
+    pk: 19,
+    name: "定番",
+    slug: "classic",
+  },
+  {
+    pk: 20,
+    name: "ゲル",
+    slug: "gel",
+  },
+  {
+    pk: 21,
+    name: "折れにくい",
+    slug: "",
+  },
+  {
+    pk: 22,
+    name: "芯径豊富",
+    slug: "variable-bold",
+  },
+  {
+    pk: 23,
+    name: "頑丈",
+    slug: "tough",
   },
 ];
