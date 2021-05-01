487a6b355241c2aeb9337f3356812f47db8e99a1
cota <65804288+cota-eng@users.noreply.github.com>
2021-05-01 21:36:28 +0900

    color change #31

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -19,7 +19,7 @@ export default function MyModal() {
         <button
           type="button"
           onClick={openModal}
-          className="px-4 py-3 text-sm font-medium text-white bg-black rounded-md bg-opacity-20 hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+          className="px-4 py-3 text-sm font-medium  bg-blue-200 text-gray-800  rounded-md bg-opacity-20 hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
         >
           Log in
         </button>
