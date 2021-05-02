d1aa7f96a83ab0ea73ba4e5835a415692c8440fd
cota <65804288+cota-eng@users.noreply.github.com>
2021-05-02 23:21:05 +0900

    :tada: SSGリニューアル #35

--- components/BrandDetail.tsx
+++ components/BrandDetail.tsx
@@ -2,21 +2,27 @@ import React from "react";
 import Link from "next/link";
 import { BRAND } from "../types/brand";
 import Image from "next/image";
-const BrandDetail: React.FC<BRAND> = ({ name, official_site_link }) => {
+
+interface Props extends BRAND {
+  category: string;
+}
+
+const BrandDetail: React.FC<Props> = ({
+  name,
+  official_site_link,
+  slug,
+  img_path,
+  description,
+  category,
+}) => {
   return (
     <div className="p-4 md:w-1/3 ">
       <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
         <img
           className="lg:h-48 md:h-36 w-full object-cover object-center"
-          src="https://dummyimage.com/720x400"
+          src={img_path}
           alt="blog"
         />
-        {/* <Image
-          src="https://dummyimage.com/720x400"
-          alt="Picture of the author"
-          width={720}
-          height={400}
-        /> */}
         <div className="p-6">
           <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
             BRAND
@@ -24,30 +30,17 @@ const BrandDetail: React.FC<BRAND> = ({ name, official_site_link }) => {
           <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
             {name}
           </h1>
-          <p className="leading-relaxed mb-3">
-            説明文が入る。説明文が入る。説明文が入る。 説明文が入る。
-            説明文が入る。 説明文が入る。 説明文が入る。
-          </p>
+          <p className="leading-relaxed mb-3">{description}</p>
           <div className="flex items-center flex-wrap ">
-            <a className="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">
-              このブランドのペン
-              <svg
-                className="w-4 h-4 ml-2"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth="2"
-                fill="none"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <path d="M5 12h14"></path>
-                <path d="M12 5l7 7-7 7"></path>
-              </svg>
-            </a>
+            <Link href={`/category/${category}/brand/${slug}/page/1`}>
+              <a className="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">
+                このブランドのペンを見る
+              </a>
+            </Link>
           </div>
           <div>
             <a href={official_site_link}>
-              公式サイト
+              <a>公式サイト</a>
               <svg
                 className="w-6 h-6 inline-block"
                 fill="none"
