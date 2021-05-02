3dc377072efc96a17fdb28acb94c54629ed61bfd
cota <65804288+cota-eng@users.noreply.github.com>
2021-05-02 20:40:49 +0900

    add data #35

--- constants/categories.ts
+++ constants/categories.ts
@@ -1,14 +1,27 @@
-export const categories = [
+interface ConstCategory {
+  name: string;
+  slug: string;
+  img_path: string;
+  description: string;
+}
+
+export const categories: ConstCategory[] = [
   {
     name: "シャーペン",
     slug: "mechanical",
+    img_path: "/category/mechanical.png",
+    description: "シャーペン紹介文",
   },
   {
     name: "ボールペン",
     slug: "ballpoint",
+    img_path: "/category/ballpoint.png",
+    description: "ボールペン紹介文",
   },
   {
     name: "万年筆",
     slug: "fountain",
+    img_path: "/category/fountain.png",
+    description: "万年筆紹介文",
   },
 ];
