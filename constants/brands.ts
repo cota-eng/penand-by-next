c2cf12c098d1fbed6a60eec52af3907ee57e0389
cota <65804288+cota-eng@users.noreply.github.com>
2021-05-02 23:56:12 +0900

    add field and data SSGリニューアル #35

--- constants/brands.ts
+++ constants/brands.ts
@@ -1,34 +1,74 @@
-export const brands = [
+interface ConstBrand {
+  name: string;
+  slug: string;
+  official_site_link: string;
+  img_path: string;
+  description: string;
+}
+
+export const brands: ConstBrand[] = [
   {
     name: "カランダッシュ",
     slug: "carandache",
+    img_path: "/brand/caran-d-ache.png",
+    official_site_link: "https://www.carandache.com/jp/",
+    description:
+      "ブランドの説明文。ブランドの説明文。ブランドの説明文。ブランドの説明文。ブランドの説明文。ブランドの説明文。ブランドの説明文。ブランドの説明文。ブランドの説明文。",
   },
   {
     name: "ファーバーカステル",
     slug: "faber-castell",
+    official_site_link: "https://www.faber-castell.jp/",
+    img_path: "/brand/faber-castell-2.png",
+    description:
+      "ブランドの説明文。ブランドの説明文。ブランドの説明文。ブランドの説明文。ブランドの説明文。ブランドの説明文。ブランドの説明文。ブランドの説明文。ブランドの説明文。",
   },
   {
     name: "ラミー",
     slug: "lamy",
+    official_site_link: "https://lamy.jp/",
+    img_path: "/brand/lamy.png",
+    description:
+      "ブランドの説明文。ブランドの説明文。ブランドの説明文。ブランドの説明文。ブランドの説明文。ブランドの説明文。ブランドの説明文。ブランドの説明文。ブランドの説明文。",
   },
   {
     name: "オート",
     slug: "ohto",
+    official_site_link: "https://ohto.co.jp/",
+    img_path: "/brand/OHTO.png",
+    description:
+      "ブランドの説明文。ブランドの説明文。ブランドの説明文。ブランドの説明文。ブランドの説明文。ブランドの説明文。ブランドの説明文。ブランドの説明文。ブランドの説明文。",
   },
   {
     name: "ぺんてる",
     slug: "pentel",
+    official_site_link: "https://www.pentel.co.jp/",
+    img_path: "/brand/pentel.png",
+    description:
+      "ブランドの説明文。ブランドの説明文。ブランドの説明文。ブランドの説明文。ブランドの説明文。ブランドの説明文。ブランドの説明文。ブランドの説明文。ブランドの説明文。",
   },
   {
     name: "トンボ",
     slug: "tombow",
+    official_site_link: "https://www.tombow.com/",
+    img_path: "/brand/tombow-pencil-logo.png",
+    description:
+      "ブランドの説明文。ブランドの説明文。ブランドの説明文。ブランドの説明文。ブランドの説明文。ブランドの説明文。ブランドの説明文。ブランドの説明文。ブランドの説明文。",
   },
   {
     name: "ユニ",
     slug: "uni",
+    official_site_link: "https://www.mpuni.co.jp/",
+    img_path: "/brand/uni-ball.png",
+    description:
+      "ブランドの説明文。ブランドの説明文。ブランドの説明文。ブランドの説明文。ブランドの説明文。ブランドの説明文。ブランドの説明文。ブランドの説明文。ブランドの説明文。",
   },
   {
     name: "ゼブラ",
     slug: "zebra",
+    official_site_link: "https://www.zebra.co.jp/",
+    img_path: "/brand/zebra-pen-company-logo.png",
+    description:
+      "ブランドの説明文。ブランドの説明文。ブランドの説明文。ブランドの説明文。ブランドの説明文。ブランドの説明文。ブランドの説明文。ブランドの説明文。ブランドの説明文。",
   },
 ];
