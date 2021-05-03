8707c92dba77c48d4230de19dcfaff3eb9d72004
cota <65804288+cota-eng@users.noreply.github.com>
2021-05-03 22:38:28 +0900

    add & change q&a #46

--- components/Question.tsx
+++ components/Question.tsx
@@ -4,22 +4,22 @@ import { ChevronUpIcon } from "@heroicons/react/solid";
 
 const Question: React.FC = () => {
   return (
-    <div className="w-full px-4 pt-16">
+    <div className="w-full px-4 pt-16 pb-10">
       <div className="w-full max-w-md p-2 mx-auto bg-white rounded-2xl">
+        <h3 className="font-semibold text-3xl my-5">よくある質問</h3>
         <Disclosure>
           {({ open }) => (
             <>
-              <Disclosure.Button className="flex justify-between w-full px-4 py-2 text-sm font-medium text-left text-purple-900 bg-purple-100 rounded-lg hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
-                <span>What is your refund policy?</span>
+              <Disclosure.Button className="flex justify-between w-full px-4 py-2 text-xl font-medium text-left  text-blue-900 bg-blue-100 rounded-lg hover:bg-blue-200 focus:outline-none focus-visible:ring focus-visible:ring-blue-500 focus-visible:ring-opacity-75">
+                <span>このサイトはいつできたのですか？</span>
                 <ChevronUpIcon
                   className={`${
                     open ? "transform rotate-180" : ""
-                  } w-5 h-5 text-purple-500`}
+                  } w-5 h-5 text-blue-500`}
                 />
               </Disclosure.Button>
               <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
-                If you're unhappy with your purchase for any reason, email us
-                within 90 days and we'll refund you in full, no questions asked.
+                2021年の5月から公開し、運用しております。
               </Disclosure.Panel>
             </>
           )}
@@ -27,16 +27,67 @@ const Question: React.FC = () => {
         <Disclosure as="div" className="mt-2">
           {({ open }) => (
             <>
-              <Disclosure.Button className="flex justify-between w-full px-4 py-2 text-sm font-medium text-left text-purple-900 bg-purple-100 rounded-lg hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
-                <span>Do you offer technical support?</span>
+              <Disclosure.Button className="flex justify-between w-full px-4 py-2 text-xl font-medium text-left text-blue-900 bg-blue-100 rounded-lg hover:bg-blue-200 focus:outline-none focus-visible:ring focus-visible:ring-blue-500 focus-visible:ring-opacity-75">
+                <span>このサイトでは何ができますか？</span>
                 <ChevronUpIcon
                   className={`${
                     open ? "transform rotate-180" : ""
-                  } w-5 h-5 text-purple-500`}
+                  } w-5 h-5 text-blue-500`}
                 />
               </Disclosure.Button>
               <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
-                No.
+                様々な文房具を閲覧、検索することができます。
+              </Disclosure.Panel>
+            </>
+          )}
+        </Disclosure>
+        <Disclosure as="div" className="mt-2">
+          {({ open }) => (
+            <>
+              <Disclosure.Button className="flex justify-between w-full px-4 py-2 text-xl font-medium text-left text-blue-900 bg-blue-100 rounded-lg hover:bg-blue-200 focus:outline-none focus-visible:ring focus-visible:ring-blue-500 focus-visible:ring-opacity-75">
+                <span>どんな機能がありますか？</span>
+                <ChevronUpIcon
+                  className={`${
+                    open ? "transform rotate-180" : ""
+                  } w-5 h-5 text-blue-500`}
+                />
+              </Disclosure.Button>
+              <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
+                【随時更新予定】ログインすることで気に入ったペンを保存したり、筆箱を作成し、値段を計算できます。（お楽しみに）
+              </Disclosure.Panel>
+            </>
+          )}
+        </Disclosure>
+        <Disclosure as="div" className="mt-2">
+          {({ open }) => (
+            <>
+              <Disclosure.Button className="flex justify-between w-full px-4 py-2 text-xl font-medium text-left text-blue-900 bg-blue-100 rounded-lg hover:bg-blue-200 focus:outline-none focus-visible:ring focus-visible:ring-blue-500 focus-visible:ring-opacity-75">
+                <span>知っているペンで載ってないものがあるのですが、、、</span>
+                <ChevronUpIcon
+                  className={`${
+                    open ? "transform rotate-180" : ""
+                  } w-5 h-5 text-blue-500`}
+                />
+              </Disclosure.Button>
+              <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
+                運営者に教えていただけると嬉しいです。
+              </Disclosure.Panel>
+            </>
+          )}
+        </Disclosure>
+        <Disclosure as="div" className="mt-2">
+          {({ open }) => (
+            <>
+              <Disclosure.Button className="flex justify-between w-full px-4 py-2 text-xl font-medium text-left text-blue-900 bg-blue-100 rounded-lg hover:bg-blue-200 focus:outline-none focus-visible:ring focus-visible:ring-blue-500 focus-visible:ring-opacity-75">
+                <span>解説がない商品が多いですが、、、</span>
+                <ChevronUpIcon
+                  className={`${
+                    open ? "transform rotate-180" : ""
+                  } w-5 h-5 text-blue-500`}
+                />
+              </Disclosure.Button>
+              <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
+                手動で書き上げ、更新しております。しばらくお待ち下さいませ。
               </Disclosure.Panel>
             </>
           )}
