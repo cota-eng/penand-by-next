492150f450ee133bc5084226a8a90fcd19b38795
cota <65804288+cota-eng@users.noreply.github.com>
2021-05-03 22:42:19 +0900

    add contetnt #46

--- components/Question.tsx
+++ components/Question.tsx
@@ -62,7 +62,9 @@ const Question: React.FC = () => {
           {({ open }) => (
             <>
               <Disclosure.Button className="flex justify-between w-full px-4 py-2 text-xl font-medium text-left text-blue-900 bg-blue-100 rounded-lg hover:bg-blue-200 focus:outline-none focus-visible:ring focus-visible:ring-blue-500 focus-visible:ring-opacity-75">
-                <span>知っているペンで載ってないものがあるのですが、、、</span>
+                <span>
+                  知っているペンで載ってないものがあると気付きましたが、どうしたらいいですか？
+                </span>
                 <ChevronUpIcon
                   className={`${
                     open ? "transform rotate-180" : ""
@@ -79,7 +81,7 @@ const Question: React.FC = () => {
           {({ open }) => (
             <>
               <Disclosure.Button className="flex justify-between w-full px-4 py-2 text-xl font-medium text-left text-blue-900 bg-blue-100 rounded-lg hover:bg-blue-200 focus:outline-none focus-visible:ring focus-visible:ring-blue-500 focus-visible:ring-opacity-75">
-                <span>解説がない商品が多いですが、、、</span>
+                <span>解説がない商品が多いですが、載せてくれますか？</span>
                 <ChevronUpIcon
                   className={`${
                     open ? "transform rotate-180" : ""
@@ -88,6 +90,23 @@ const Question: React.FC = () => {
               </Disclosure.Button>
               <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
                 手動で書き上げ、更新しております。しばらくお待ち下さいませ。
+              </Disclosure.Panel>
+            </>
+          )}
+        </Disclosure>
+        <Disclosure as="div" className="mt-2">
+          {({ open }) => (
+            <>
+              <Disclosure.Button className="flex justify-between w-full px-4 py-2 text-xl font-medium text-left text-blue-900 bg-blue-100 rounded-lg hover:bg-blue-200 focus:outline-none focus-visible:ring focus-visible:ring-blue-500 focus-visible:ring-opacity-75">
+                <span>サイトづくりに協力したいです</span>
+                <ChevronUpIcon
+                  className={`${
+                    open ? "transform rotate-180" : ""
+                  } w-5 h-5 text-blue-500`}
+                />
+              </Disclosure.Button>
+              <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
+                ありがとうございます。お気軽にご連絡ください。
               </Disclosure.Panel>
             </>
           )}
