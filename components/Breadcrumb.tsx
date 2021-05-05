6e30704df086450a28bc77ea1bdd77d4a7d37863
cota <65804288+cota-eng@users.noreply.github.com>
2021-05-05 15:32:24 +0900

    implemented #53

--- components/Breadcrumb.tsx
+++ components/Breadcrumb.tsx
@@ -1,6 +1,13 @@
-import React from "react";
 import Link from "next/link";
-const Breadcrumb: React.FC = () => {
+
+interface Bread {
+  name: string;
+  slug: string;
+}
+interface Props {
+  breads: Bread[];
+}
+const Breadcrumb: React.FC<Props> = ({ breads }) => {
   return (
     <nav className="text-black font-bold ml-10 my-8" aria-label="Breadcrumb">
       <ol className="list-none p-0 inline-flex">
@@ -8,30 +15,33 @@ const Breadcrumb: React.FC = () => {
           <Link href="/">
             <a className="underline font-semibold">
               Home
-              <svg
+              {/* <svg
                 className="fill-current w-3 h-3 mx-3 inline"
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 320 512"
               >
                 <path d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z" />
-              </svg>
+              </svg> */}
             </a>
           </Link>
         </li>
-        <li className="flex items-center">
-          <Link href="/">
-            <a className="underline font-semibold">
-              Second Level
-              <svg
-                className="fill-current w-3 h-3 mx-3 inline"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 320 512"
-              >
-                <path d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z" />
-              </svg>
-            </a>
-          </Link>
-        </li>
+        {breads &&
+          breads.map((bread) => (
+            <li className="flex items-center">
+              <Link href={`${bread.slug}`}>
+                <a className="underline font-semibold">
+                  <svg
+                    className="fill-current w-3 h-3 mx-3 inline"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 320 512"
+                  >
+                    <path d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z" />
+                  </svg>
+                  {bread.name}
+                </a>
+              </Link>
+            </li>
+          ))}
       </ol>
     </nav>
   );
