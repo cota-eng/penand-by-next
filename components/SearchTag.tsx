cc9b77113ea829758c5d6e5f079d213d9ee49285
cota <65804288+cota-eng@users.noreply.github.com>
2021-05-06 21:37:32 +0900

    change #62

--- components/SearchTag.tsx
+++ components/SearchTag.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import React from "react";
 import { tags } from "../constants/tags";
-
+import FadeIn from "react-fade-in";
 const SearchTag: React.FC = () => {
   return (
     <>
@@ -11,27 +11,29 @@ const SearchTag: React.FC = () => {
             key={index}
             className="xl:w-1/4 ls:w-1/4 md:w-1/4 w-1/2 my-4 px-4 bg-white rounded-lg shadow-lg dark:bg-gray-800"
           >
-            <div className="py-5 text-center">
-              <Image
-                className="object-cover mx-auto"
-                src={tag.image}
-                height={100}
-                width={100}
-                alt="search tag"
-                quality={20}
-              />
-            </div>
-            <div className="py-5 text-center">
-              <a
-                href="#"
-                className="block text-2xl font-bold text-gray-800 dark:text-white"
-              >
-                {tag.name}
-              </a>
-              <span className="text-sm text-gray-700 dark:text-gray-200">
-                {tag.slug}
-              </span>
-            </div>
+            <FadeIn>
+              <div className="py-2 text-center">
+                <Image
+                  className="object-cover mx-auto"
+                  src={tag.image}
+                  height={80}
+                  width={80}
+                  alt="search tag"
+                  quality={20}
+                />
+              </div>
+              <div className="py-2 text-center">
+                <a
+                  href="#"
+                  className="block text-2xl font-bold text-gray-800 dark:text-white"
+                >
+                  {tag.name}
+                </a>
+                <span className="text-sm text-gray-700 dark:text-gray-200">
+                  {tag.slug}
+                </span>
+              </div>
+            </FadeIn>
           </div>
         ))}
     </>
