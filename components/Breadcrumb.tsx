1f30ffcffe7e6b0db35a2b13d775abaed8d1f236
cota <65804288+cota-eng@users.noreply.github.com>
2021-05-23 20:52:21 +0900

    delete unuse

--- components/Breadcrumb.tsx
+++ components/Breadcrumb.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-
+import React from "react";
 interface Bread {
   name: string;
   slug: string;
@@ -13,16 +13,7 @@ const Breadcrumb: React.FC<Props> = ({ breads }) => {
       <ol className="flex list-none p-0 flex-wrap">
         <li className="flex items-center">
           <Link href="/">
-            <a className="underline font-semibold">
-              Home
-              {/* <svg
-                className="fill-current w-3 h-3 mx-3 inline"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 320 512"
-              >
-                <path d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z" />
-              </svg> */}
-            </a>
+            <a className="underline font-semibold">Home</a>
           </Link>
         </li>
         {breads &&
@@ -47,4 +38,4 @@ const Breadcrumb: React.FC<Props> = ({ breads }) => {
   );
 };
 
-export default Breadcrumb;
+export default React.memo(Breadcrumb);
