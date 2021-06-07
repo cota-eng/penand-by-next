69c8bb2c26cb67043a30d408394d4587ed04b795
cota <65804288+cota-eng@users.noreply.github.com>
2021-06-07 11:04:10 +0900

    number array for key

--- components/ResultSkelton.tsx
+++ components/ResultSkelton.tsx
@@ -2,24 +2,23 @@ import React from "react";
 import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 
 const ResultSkelton: React.FC = () => {
+  const numbers = [...Array(6)].map((_, i) => i);
   return (
     <>
-      {Array(10)
-        .fill(0)
-        .map((i) => (
-          <div className="p-2 lg:w-1/2 md:w-1/2 sm:w-1/2 w-full cursor-pointer">
-            <div
-              key={i}
-              className="max-w-2xl bg-blue-50 border-1SS border-gray-300 px-5 py-3 rounded-md tracking-wide shadow-lg m-2"
-            >
-              <div className="flex">
-                <Skeleton width={140} height={140} />
-                <div className="flex flex-col justify-between ml-3 w-1/2">
-                  <h3 className=" font-semibold mb-2">
-                    <Skeleton count={1} width={200} height={20} />
-                  </h3>
-                  <p className="text-gray-800 mt-2">
-                    {/* <span
+      {numbers.map((i) => (
+        <div
+          key={i}
+          className="p-2 lg:w-1/2 md:w-1/2 sm:w-1/2 w-full cursor-pointer"
+        >
+          <div className="max-w-2xl bg-blue-50 border-1SS border-gray-300 px-5 py-3 rounded-md tracking-wide shadow-lg m-2">
+            <div className="flex">
+              <Skeleton width={140} height={140} />
+              <div className="flex flex-col justify-between ml-3 w-1/2">
+                <h3 className=" font-semibold mb-2">
+                  <Skeleton count={1} width={200} height={20} />
+                </h3>
+                <p className="text-gray-800 mt-2">
+                  {/* <span
                       className="inline-block rounded-full text-white 
                 bg-black hover:bg-gray-500 duration-300 
                 text-xs font-bold 
@@ -27,13 +26,13 @@ const ResultSkelton: React.FC = () => {
                 opacity-90 hover:opacity-100"
                     >
                 </span> */}
-                    <Skeleton count={4} />
-                  </p>
-                </div>
+                  <Skeleton count={4} />
+                </p>
               </div>
             </div>
           </div>
-        ))}
+        </div>
+      ))}
     </>
   );
 };
